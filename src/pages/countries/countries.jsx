739aa5efe611f275015c664ec7bf7ee076529606
eminebozdag <fyrs00@hotmail.com>
2022-10-17 20:19:47 +0200

    added css

--- src/pages/countries/countries.jsx
+++ src/pages/countries/countries.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Search from "../../components/search/search";
 import "./countries.css";
 import Country from "./country/country";
 const Countries = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  const fetchData = async () => {
+    const API_URL = "https://restcountries.com/v2/all";
+    try {
+      const response = await axios.get(API_URL);
+      const data = await response.data;
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="countries-search-container">
       <Search />
       <div className="countries-container">
-        <Country />
-        <Country />
-        <Country />
-        <Country />
+        {data.map((country) => (
+          <Country data={country} />
+        ))}
       </div>
     </div>
   );
--- src/pages/countries/country/country.jsx
+++ src/pages/countries/country/country.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 import "./country.css";
-const Country = () => {
-  return <div className="country-container">country</div>;
+const Country = (props) => {
+  const { name, flag, capital, languages, population, currencies } = props.data;
+
+  const formattedLanguages = languages
+    .map((language) => language.name)
+    .join(", ");
+
+  const formattedCurrencies =
+    currencies !== undefined &&
+    currencies.map((currency) => currency?.name || "").join(", ");
+
+  return (
+    <div className="country-container">
+      <div className="country-flag">
+        <img src={flag} alt="flag" className="flag-img" />
+      </div>
+      <div className="country-info">
+        <h2>{name}</h2>
+        <p>
+          Capital: <span className="info-span">{capital}</span>
+        </p>
+        <p>
+          Language: <span className="info-span">{formattedLanguages}</span>
+        </p>
+        <p>
+          Population:{" "}
+          <span className="info-span">
+            {population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </span>
+        </p>
+        <p>
+          Currency: <span className="info-span">{formattedCurrencies}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Country;
